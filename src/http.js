22a72552540467900473cb29c45e1136a1d3eec0
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-07-02 14:11:51 +0200

    upgrade chlu-ipfs to version with unverified reviews support

--- src/http.js
+++ src/http.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const ChluIPFSDID = require('./ipfs-did')
-const { isCID } = require('./ipfs')
 const log = require('./log')
 
-function getWebServer(chluIpfs, db, token) {
-    const chluIpfsDID = new ChluIPFSDID(chluIpfs, db)
+function getWebServer(chluIpfs, token) {
+    const chluIpfsDID = new ChluIPFSDID(chluIpfs)
     const app = express()
     app.get('/', (req, res) => res.send('Chlu DID Service').end())
     app.get('/did/:didId', async (req, res) => {
@@ -21,27 +20,13 @@ function getWebServer(chluIpfs, db, token) {
             }
         }
     })
-    app.get('/did', async (req, res) => {
-        if (token && token !== req.query.token) {
-            res.status(400).send('Missing API token').end()
-        } else {
-            try {
-                const didList = await chluIpfsDID.getDIDList()
-                res.json(didList)
-            } catch (error) {
-                log(error)
-                res.status(500).send(error.message).end()
-            }
-        }
-
-    })
     app.get('/reputation/:didId', async (req, res) => {
         if (token && token !== req.query.token) {
             res.status(400).send('Missing API token').end()
         } else {
             try {
                 const didId = req.params.didId
-                const ddo = await chluIpfsDID.getUnverifiedReviews(didId)
+                const ddo = await chluIpfsDID.getReviewsByDID(didId)
                 res.json(ddo)
             } catch (error) {
                 log(error)
@@ -57,10 +42,10 @@ function getWebServer(chluIpfs, db, token) {
                 const { didId, data, signature } = req.params
                 const valid = await chluIpfsDID.verifyUsingDID(didId, data, signature)
                 if (valid) {
-                    const ddo = await chluIpfsDID.getUnverifiedReviews(didId)
+                    const publicDidDocument = await chluIpfsDID.getDID(didId)
                     res.json({
                         valid,
-                        ddo
+                        publicDidDocument
                     })
                 }
             } catch (error) {
